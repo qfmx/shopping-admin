5ab1cbbe6b1b064e5a1678227d8869a21199bfe9
qfmx <1125438556@qq.com>
2019-06-21 08:59:32 +0800

    大量配置更改与细化

--- springcloud-02-shopping-config-server/src/main/java/com/springcloud/SpringCloudConfigServer.java
+++ springcloud-02-shopping-config-server/src/main/java/com/springcloud/SpringCloudConfigServer.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
-public class MySpringCloudConfig {
+public class SpringCloudConfigServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringCloudConfig.class, args);
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringCloudConfigServer.class, args);
 	}
+
 
 }
--- springcloud-02-shopping-order-provider/src/main/java/com/springcloud/SpringCloudOrdersProvider.java
+++ springcloud-02-shopping-order-provider/src/main/java/com/springcloud/SpringCloudOrdersProvider.java
@@ -3,14 +3,15 @@ package com.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @MapperScan("com.springcloud.dao")
 public class SpringCloudOrdersProvider {
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringCloudOrdersProvider.class, args);
 	}
+
 }
